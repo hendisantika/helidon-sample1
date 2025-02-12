bcd417ab16945ee83b722de9b5358d36f147fcf6
hendisantika <hendisantika@yahoo.co.id>
2025-02-12 14:23:36 +0700

    feat: Add PokemonResource🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/helidonsample1/PokemonResource.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.helidonsample1;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.Path;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : helidon-sample1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/02/25
+ * Time: 14.23
+ * To change this template use File | Settings | File Templates.
+ */
+@ApplicationScoped
+@Path("/pokemons")
+public class PokemonResource {
+    private final PokemonService pokemonService;
+    private final Integer defaultLimit = 5;
+}
