985ce0e861d095d3f1160c43a45ff6df294b0130
hendisantika <hendisantika@yahoo.co.id>
2025-02-12 14:24:21 +0700

    feat: Add defaultList in PokemonResource🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/helidonsample1/PokemonResource.java
+++ src/main/java/id/my/hendisantika/helidonsample1/PokemonResource.java
@@ -2,7 +2,12 @@ package id.my.hendisantika.helidonsample1;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.ArrayList;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,5 +29,13 @@ public class PokemonResource {
     @Inject
     public PokemonResource(PokemonService pokemonService) {
         this.pokemonService = pokemonService;
+    }
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String defaultList() throws Exception {
+        ArrayList<Object> pokemons = pokemonService.getPokemons(defaultLimit);
+        return getResult(pokemons);
     }
 }
